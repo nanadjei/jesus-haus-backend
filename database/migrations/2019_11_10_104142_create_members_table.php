39097eda83501baf84ef3cb943cdcd2d4be06e8b
= Nana Adjei <nana.elvee@gmail.com>
2019-11-10 11:04:13 +0000

    Migrations created

--- database/migrations/2019_11_10_104142_create_members_table.php
+++ database/migrations/2019_11_10_104142_create_members_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateUsersTable extends Migration
+class CreateMembersTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,18 +13,18 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('members', function (Blueprint $table) {
             $table->bigIncrements('id');
+            $table->unsignedBigInteger("department_id")->comment("The role that the user has");
+            $table->foreign("department_id")->references("id")->on("departments");
 
-            $table->unsignedBigInteger("role_id")->comment("The role that the user has");
-            $table->foreign("role_id")->references("id")->on("roles");
-
+            $table->enum('type', ["member", "visitor"])->default("member");
             $table->string('first_name');
             $table->string('last_name')->nullable();
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->enum('title', ["mr", "mrs", "miss"]);
+            $table->date("dob")->nullable();
+            $table->string('phone')->nullable();
             $table->timestamps();
         });
     }
@@ -36,6 +36,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('members');
     }
 }
---
+++ database/migrations/2019_11_10_105159_create_attendances_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAttendancesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('attendances', function (Blueprint $table) {
+            $table->bigIncrements('id');
+
+            $table->unsignedBigInteger("member_id")->comment("The id of the member acting as a referecnce");
+            $table->foreign("member_id")->references("id")->on("members");
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('attendances');
+    }
+}
